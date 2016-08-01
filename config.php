c348102fc649eb312918a7a2f9de87d908e95d4c
Kyle Ladd <kladd6@gmail.com>
2016-07-31 20:05:45 -0400

    Trello portion for creating cards can be set up using the custom form page

--- config.php
+++ config.php
@@ -5,30 +5,43 @@ require_once(INCLUDE_DIR.'/class.forms.php');
 require_once(INCLUDE_DIR.'/class.dept.php');
  
  class TrelloConfig extends PluginConfig{
-// class TrelloConfig implements PluginCustomConfig{
 	function hasCustomConfig(){
-		// echo "HAS CUSTOM CONFIG";
 		return true;
 	}
 	function renderCustomConfig(){
-		echo "HERE IS THE FORM";
 		?>
+		<br>
+		<button id="fetchBoards">Fetch Trello Boards</button><br>
+		<button id="fetchLists">Fetch Trello Lists</button><br>
 		<script>
-		alert("hi");
+		$(function() {
+			$("#fetchBoards").click(function(e){
+				e.preventDefault();
+				$.getJSON("https://api.trello.com/1/members/me/boards?key="+$("[name='trello_api_key']").val()+"&token="+$("[name='trello_api_token']").val(),function(data){
+					console.log("BOARDS",data);
+				})
+				.fail(function(){
+					alert("Failed to get boards");
+				});
+			});
+			$("#fetchLists").click(function(e){
+				e.preventDefault();
+				$.getJSON("https://api.trello.com/1/boards/"+$("[name='trello_board_id']").val()+"/lists?key="+$("[name='trello_api_key']").val()+"&token="+$("[name='trello_api_token']").val(),function(data){
+					console.log("LISTS",data);
+				})
+				.fail(function(){
+					alert("Failed to get Lists");
+				});
+			});
+		});
 		</script>
 		<?php
-		// return "HERE IS A FORM";
 		$form = $this->getForm();
 		$form->render();
-		// if ($form && $_POST)
-  //           $form->isValid();
 	}
 
 	function saveCustomConfig(){
-		// $form = $this->getForm();
 		return $this->commitForm();
-		// return $form->isValid();
-		// return $this->pre_save();
 	}
 
 	function getOptions() {
@@ -53,6 +66,26 @@ require_once(INCLUDE_DIR.'/class.dept.php');
 		 	'desc' => 'Get your Token: https://trello.com/1/authorize?key=APPLICATIONKEYHERE&scope=read%2Cwrite&name=My+Application&expiration=never&response_type=token'
 		 	),
 		 )),
+	 	'trello_board_id' => new TextboxField(array(
+		 'id' => 'trello_board_id',
+		 'label' => 'Trello Board ID',
+		 'required'=>true,
+		 'hint'=>__('Get your Token: https://trello.com/1/authorize?key=APPLICATIONKEYHERE&scope=read%2Cwrite&name=My+Application&expiration=never&response_type=token'),
+		 'configuration' => array(
+		 	'length' => 0,
+		 	'desc' => 'Get your Token: https://trello.com/1/authorize?key=APPLICATIONKEYHERE&scope=read%2Cwrite&name=My+Application&expiration=never&response_type=token'
+		 	),
+		 )),
+	 	'trello_list_id' => new TextboxField(array(
+		 'id' => 'trello_list_id',
+		 'label' => 'Trello Creation List ID',
+		 'required'=>true,
+		 'hint'=>__('When a ticket is created, add card to this list'),
+		 'configuration' => array(
+		 	'length' => 0,
+		 	'desc' => 'When a ticket is created, add card to this list'
+		 	),
+		 )),
 		'osticket_department_id' => new ChoiceField(array(
             'id'=>'osticket_department_id',
             'label'=>__('Department'),
@@ -62,26 +95,7 @@ require_once(INCLUDE_DIR.'/class.dept.php');
             'configuration'=>array(
                 'multiselect' => false,
             )
-        )),
-  		// // https://developers.trello.com/advanced-reference/
-		// use Trello\Client;
-		// $client = new Client();
-		// $client->authenticate($key, $token, Client::AUTH_URL_CLIENT_ID);
-		// // Get All boards for username
-		// $boards = $client->api('member')->boards()->all("TRELLOUSERNAME");
-		// // Get all lists on board by Board ID
-		// $lists = $client->boards()->lists()->all("BOARDID");
-
-        'trello_list_id' => new TextboxField(array(
-		 'id' => 'trello_list_id',
-		 'label' => 'Trello Creation List ID',
-		 'required'=>true,
-		 'hint'=>__('When a ticket is created, add card to this list'),
-		 'configuration' => array(
-		 	'length' => 0,
-		 	'desc' => 'When a ticket is created, add card to this list'
-		 	),
-		 ))
+        ))
 	 );
  }
  
