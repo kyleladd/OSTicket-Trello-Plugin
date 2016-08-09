16b73c7fd2ef2325308de4d5b984ef1bc4e39c08
Kyle Ladd <kladd6@gmail.com>
2016-08-09 19:15:04 -0400

    Use OSTicket's URL as Base to OSTicket Trello API Route

--- config.php
+++ config.php
@@ -40,135 +40,134 @@ class OptionalValidationChoicesWidget extends ChoicesWidget{
     }
 }
  class TrelloConfig extends PluginConfig{
-	function hasCustomConfig(){
-		return true;
-	}
-	function renderCustomConfig(){
-		$form = $this->getForm();
+    function hasCustomConfig(){
+        return true;
+    }
+    function renderCustomConfig(){
+        $form = $this->getForm();
             $form->isValid();
-		?>
-		<script>
-		$(function() {
-			$('<button id="fetchBoards" style="margin-left:5px;">Fetch Trello Boards</button>').insertAfter($("[name='trello_board_id[]']"));
-			$('<button id="fetchLists" style="margin-left:5px;">Fetch Trello Lists</button>').insertAfter($("[name='trello_list_id[]']"));
-			$("#fetchBoards").click(function(e){
-				e.preventDefault();
-				$.getJSON("https://api.trello.com/1/members/me/boards?filter=open&key="+$("[name='trello_api_key']").val()+"&token="+$("[name='trello_api_token']").val(),function(data){
-					var result = "";
-					for(var i in data){
-						var board = data[i];
-						result += "<option value=\"" + board.id + "\" " + ("<?=$form->getField("trello_board_id")->value;?>" === board.id ? "selected=\"selected\"" : "" ) + ">" + board.name + "</option>";
-					}
-					$("[name='trello_board_id[]']").html(result);
-				})
-				.fail(function(){
-					alert("Failed to get Trello Boards. Be sure to fill out the Trello API Keyand Trello API Token first.");
-				});
-			});
-			$("#fetchLists").click(function(e){
-				e.preventDefault();
-				$.getJSON("https://api.trello.com/1/boards/"+$("[name='trello_board_id[]']").val()+"/lists/?filter=open&key="+$("[name='trello_api_key']").val()+"&token="+$("[name='trello_api_token']").val(),function(data){
-					var result = "";
-					for(var i in data){
-						var list = data[i];
-						result += "<option value=\"" + list.id + "\" " + ("<?=$form->getField("trello_list_id")->value;?>" === list.id ? "selected=\"selected\"" : "" ) + ">" + list.name + "</option>";
-					}
-					$("[name='trello_list_id[]']").html(result);
-				})
-				.fail(function(){
-					alert("Failed to get Trello Lists. Be sure to fill out the Trello API Key, Trello API Token, and Trello Board first.");
-				});
-			});
-		});
-		</script>
-		<?php
-		$form->render();
-	}
+        ?>
+        <script>
+        $(function() {
+            $('<button id="fetchBoards" style="margin-left:5px;">Fetch Trello Boards</button>').insertAfter($("[name='trello_board_id[]']"));
+            $('<button id="fetchLists" style="margin-left:5px;">Fetch Trello Lists</button>').insertAfter($("[name='trello_list_id[]']"));
+            $("#fetchBoards").click(function(e){
+                e.preventDefault();
+                $.getJSON("https://api.trello.com/1/members/me/boards?filter=open&key="+$("[name='trello_api_key']").val()+"&token="+$("[name='trello_api_token']").val(),function(data){
+                    var result = "";
+                    for(var i in data){
+                        var board = data[i];
+                        result += "<option value=\"" + board.id + "\" " + ("<?=$form->getField("trello_board_id")->value;?>" === board.id ? "selected=\"selected\"" : "" ) + ">" + board.name + "</option>";
+                    }
+                    $("[name='trello_board_id[]']").html(result);
+                })
+                .fail(function(){
+                    alert("Failed to get Trello Boards. Be sure to fill out the Trello API Keyand Trello API Token first.");
+                });
+            });
+            $("#fetchLists").click(function(e){
+                e.preventDefault();
+                $.getJSON("https://api.trello.com/1/boards/"+$("[name='trello_board_id[]']").val()+"/lists/?filter=open&key="+$("[name='trello_api_key']").val()+"&token="+$("[name='trello_api_token']").val(),function(data){
+                    var result = "";
+                    for(var i in data){
+                        var list = data[i];
+                        result += "<option value=\"" + list.id + "\" " + ("<?=$form->getField("trello_list_id")->value;?>" === list.id ? "selected=\"selected\"" : "" ) + ">" + list.name + "</option>";
+                    }
+                    $("[name='trello_list_id[]']").html(result);
+                })
+                .fail(function(){
+                    alert("Failed to get Trello Lists. Be sure to fill out the Trello API Key, Trello API Token, and Trello Board first.");
+                });
+            });
+        });
+        </script>
+        <?php
+        $form->render();
+    }
 
-	function saveCustomConfig(){
-		try{
-			$config = TrelloPlugin::getConfig();
-			// Initial board
-			$initial_board = $config->get('trello_board_id');
-			if($this->commitForm()){
-				$config = TrelloPlugin::getConfig();
-				$saved_board = $config->get('trello_board_id');
-				// Create webhook for new board and remove webhook from old board if there is one
-				if($saved_board !== $initial_board || $config->get('trello_webhook_id') === ""){
-					$client = new Client();
-	                $client->authenticate($config->get('trello_api_key'), $config->get('trello_api_token'), Client::AUTH_URL_CLIENT_ID);
-	                if($config->get('trello_webhook_id')!==""){
-	                	// Remove existing webhook
-            			try{
-		                	$client->webhooks()->remove($config->get('trello_webhook_id'));
-			            }
-            			catch(Exception $e){
-					        error_log("Unable to delete Trello Webhook. " . $e->getMessage());
-					        echo "Unable to delete Trello Webhook.";
-					        var_dump($e);
-					    }
-	                }
-                    // http://136.204.104.63:8080/osticket/api/trello
-                    // http://kyleladd.us/phppost/
-	                $trello_webhook_create = $client->webhooks()->create(array("idModel"=>$saved_board,"callbackURL"=>"http://136.204.104.63:8080/osticket/api/trello","description"=>"OSTicket Plugin"));
-	                if(TrelloConfig::update('trello_webhook_id',$trello_webhook_create['id']) === false){
-	                	echo "Failed to save created webhook to database";
-	                	return false;
-	                }
-				}
+    function saveCustomConfig(){
+        try{
+            global $cfg;
+            $config = TrelloPlugin::getConfig();
+            // Initial board
+            $initial_board = $config->get('trello_board_id');
+            if($this->commitForm()){
+                $config = TrelloPlugin::getConfig();
+                $saved_board = $config->get('trello_board_id');
+                // Create webhook for new board and remove webhook from old board if there is one
+                if($saved_board !== $initial_board || $config->get('trello_webhook_id') === ""){
+                    $client = new Client();
+                    $client->authenticate($config->get('trello_api_key'), $config->get('trello_api_token'), Client::AUTH_URL_CLIENT_ID);
+                    if($config->get('trello_webhook_id')!==""){
+                        // Remove existing webhook
+                        try{
+                            $client->webhooks()->remove($config->get('trello_webhook_id'));
+                        }
+                        catch(Exception $e){
+                            error_log("Unable to delete Trello Webhook. " . $e->getMessage());
+                            echo "Unable to delete Trello Webhook.";
+                            var_dump($e);
+                        }
+                    }
+                    $trello_webhook_create = $client->webhooks()->create(array("idModel"=>$saved_board,"callbackURL"=>$cfg->getBaseUrl() . "/api/trello","description"=>"OSTicket Plugin"));
+                    if(TrelloConfig::update('trello_webhook_id',$trello_webhook_create['id']) === false){
+                        echo "Failed to save created webhook to database";
+                        return false;
+                    }
+                }
 
-			}
-		}
-		catch(Exception $e){
-	        error_log("Error authenticating to Trello. " . $e->getMessage());
-	        var_dump($e);
-	        return false;
-	    }
-		return true;
-	}
+            }
+        }
+        catch(Exception $e){
+            error_log("Error authenticating to Trello. " . $e->getMessage());
+            var_dump($e);
+            return false;
+        }
+        return true;
+    }
 
-	function getOptions() {
-	  return array(
-	 	'trello_api_key' => new TextboxField(array(
-		 'id' => 'trello_api_key',
-		 'label' => 'Trello API Key',
-		 'required'=>true,
-		 'hint'=>__('Get your Key: https://trello.com/app-key'),
-		 'configuration' => array(
-		 	'length' => 0,
-		 	'desc' => 'Get your Key: https://trello.com/app-key'
-		 	)
-		 )),
-	 	'trello_api_token' => new TextboxField(array(
-		 'id' => 'trello_api_token',
-		 'label' => 'Trello API Token',
-		 'required'=>true,
-		 'hint'=>__('Get your Token: https://trello.com/1/authorize?key=APPLICATIONKEYHERE&scope=read%2Cwrite&name=My+Application&expiration=never&response_type=token'),
-		 'configuration' => array(
+    function getOptions() {
+      return array(
+        'trello_api_key' => new TextboxField(array(
+         'id' => 'trello_api_key',
+         'label' => 'Trello API Key',
+         'required'=>true,
+         'hint'=>__('Get your Key: https://trello.com/app-key'),
+         'configuration' => array(
+            'length' => 0,
+            'desc' => 'Get your Key: https://trello.com/app-key'
+            )
+         )),
+        'trello_api_token' => new TextboxField(array(
+         'id' => 'trello_api_token',
+         'label' => 'Trello API Token',
+         'required'=>true,
+         'hint'=>__('Get your Token: https://trello.com/1/authorize?key=APPLICATIONKEYHERE&scope=read%2Cwrite&name=My+Application&expiration=never&response_type=token'),
+         'configuration' => array(
             'length' => 0
             ),
-		 )),
-	 	'trello_board_id' => new OptionalValidationChoiceField(array(
-		 'id' => 'trello_board_id',
-		 'label' => 'Trello Board ID',
-		 'required'=>true,
-		 'hint'=>__('Get your Token: https://trello.com/1/authorize?key=APPLICATIONKEYHERE&scope=read%2Cwrite&name=My+Application&expiration=never&response_type=token'),
-		 'configuration' => array(
-		 	'multiselect' => false,
-		 	'validate_choices' => false
-		 	),
-		 )),
-	 	'trello_list_id' => new OptionalValidationChoiceField(array(
-		 'id' => 'trello_list_id',
-		 'label' => 'Trello Creation List ID',
-		 'required'=>true,
-		 'hint'=>__('When a ticket is created, add card to this list'),
-		 'configuration' => array(
-		 	'multiselect' => false,
-		 	'validate_choices' => false
-		 	),
-		 )),
-		'osticket_department_id' => new ChoiceField(array(
+         )),
+        'trello_board_id' => new OptionalValidationChoiceField(array(
+         'id' => 'trello_board_id',
+         'label' => 'Trello Board ID',
+         'required'=>true,
+         'hint'=>__('Get your Token: https://trello.com/1/authorize?key=APPLICATIONKEYHERE&scope=read%2Cwrite&name=My+Application&expiration=never&response_type=token'),
+         'configuration' => array(
+            'multiselect' => false,
+            'validate_choices' => false
+            ),
+         )),
+        'trello_list_id' => new OptionalValidationChoiceField(array(
+         'id' => 'trello_list_id',
+         'label' => 'Trello Creation List ID',
+         'required'=>true,
+         'hint'=>__('When a ticket is created, add card to this list'),
+         'configuration' => array(
+            'multiselect' => false,
+            'validate_choices' => false
+            ),
+         )),
+        'osticket_department_id' => new ChoiceField(array(
             'id'=>'osticket_department_id',
             'label'=>__('Department'),
             'required'=>true,
@@ -178,25 +177,25 @@ class OptionalValidationChoicesWidget extends ChoicesWidget{
                 'multiselect' => false
             )
         )),
-	 	'trello_webhook_id' => new TextboxField(array(
-		 'id' => 'trello_webhook_id',
-		 'label' => 'Current Trello Webhook',
-		 'required'=>false,
-		 'hint'=>__('Generated and used for webhook removal'),
-		 'configuration' => array(
+        'trello_webhook_id' => new TextboxField(array(
+         'id' => 'trello_webhook_id',
+         'label' => 'Current Trello Webhook',
+         'required'=>false,
+         'hint'=>__('Generated and used for webhook removal'),
+         'configuration' => array(
             'length' => 0
             ),
-		 ))
-	 );
+         ))
+     );
  }
 
  function pre_save(&$config, &$errors) {
-	global $msg;
+    global $msg;
 
-	if (!$errors)
-	  $msg = 'Configuration updated successfully';
+    if (!$errors)
+      $msg = 'Configuration updated successfully';
 
-	return true;
+    return true;
  }
 }
 ?>
--- trello.php
+++ trello.php
@@ -111,22 +111,4 @@ class TrelloPlugin extends Plugin {
         $trelloIPs = array("107.23.104.115","107.23.149.70","54.152.166.250","54.164.77.56");
         return in_array($ip,$trelloIPs);
     }
-
-    static function url_origin( $s, $use_forwarded_host = false )
-    {
-        $ssl      = ( ! empty( $s['HTTPS'] ) && $s['HTTPS'] == 'on' );
-        $sp       = strtolower( $s['SERVER_PROTOCOL'] );
-        $protocol = substr( $sp, 0, strpos( $sp, '/' ) ) . ( ( $ssl ) ? 's' : '' );
-        $port     = $s['SERVER_PORT'];
-        $port     = ( ( ! $ssl && $port=='80' ) || ( $ssl && $port=='443' ) ) ? '' : ':'.$port;
-        $host     = ( $use_forwarded_host && isset( $s['HTTP_X_FORWARDED_HOST'] ) ) ? $s['HTTP_X_FORWARDED_HOST'] : ( isset( $s['HTTP_HOST'] ) ? $s['HTTP_HOST'] : null );
-        $host     = isset( $host ) ? $host : $s['SERVER_NAME'] . $port;
-        return $protocol . '://' . $host;
-    }
-
-    static function full_url( $s, $use_forwarded_host = false )
-    {
-        return url_origin( $s, $use_forwarded_host ) . $s['REQUEST_URI'];
-    }
-
 }
