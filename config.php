2838b797a399a89a8919b00b2f45cc9a1e2e211c
Kyle Ladd <kladd@sidearmsports.internetconsult.com>
2016-12-21 12:52:05 -0500

    FormField->get default is null

--- config.php
+++ config.php
@@ -95,8 +95,7 @@ class OptionalValidationChoicesWidget extends ChoicesWidget{
                 $config = TrelloPlugin::getConfig();
                 $saved_board = $config->get('trello_board_id');
                 // Create webhook for new board and remove webhook from old board if there is one
-                if($saved_board !== $initial_board || $config->get('trello_webhook_id') === ""){
-                    echo "creating webhook";
+                if($saved_board !== $initial_board || $config->get('trello_webhook_id') === null || $config->get('trello_webhook_id') === ""){
                     $client = new Client();
                     $client->authenticate($config->get('trello_api_key'), $config->get('trello_api_token'), Client::AUTH_URL_CLIENT_ID);
                     if($config->get('trello_webhook_id') !== ""){
